5734a6e28ee3ca59054d709809f696d327925fc4
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-08-21 09:18:10 -0300

    Updates

---
+++ precache-manifest.61c2a89c7c66f0d4459614cc84a21e32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f3fa2c9659ad9150064a9790bd017b",
+    "url": "/project-trybewallet-trybe/index.html"
+  },
+  {
+    "revision": "341d878b6405180a4d4c",
+    "url": "/project-trybewallet-trybe/static/css/main.a90189a8.chunk.css"
+  },
+  {
+    "revision": "b96fa86e39ca0cd2e227",
+    "url": "/project-trybewallet-trybe/static/js/2.6a1dc678.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-trybewallet-trybe/static/js/2.6a1dc678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341d878b6405180a4d4c",
+    "url": "/project-trybewallet-trybe/static/js/main.1a8ab7a0.chunk.js"
+  },
+  {
+    "revision": "6503f4fa0966a5dcba5e",
+    "url": "/project-trybewallet-trybe/static/js/runtime-main.6f31c4ca.js"
+  }
+]);
