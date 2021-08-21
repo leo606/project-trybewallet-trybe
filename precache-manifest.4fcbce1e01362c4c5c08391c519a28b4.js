05a07b74db1e69928596d738d7798ad678edd034
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-08-21 09:28:18 -0300

    Updates

--- precache-manifest.4fcbce1e01362c4c5c08391c519a28b4.js
+++ precache-manifest.4fcbce1e01362c4c5c08391c519a28b4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "59f3fa2c9659ad9150064a9790bd017b",
+    "revision": "b1af267e584c153ae386d99faeaced3e",
     "url": "/project-trybewallet-trybe/index.html"
   },
   {
-    "revision": "341d878b6405180a4d4c",
+    "revision": "dfd79d317ccb2a845311",
     "url": "/project-trybewallet-trybe/static/css/main.a90189a8.chunk.css"
   },
   {
-    "revision": "b96fa86e39ca0cd2e227",
-    "url": "/project-trybewallet-trybe/static/js/2.6a1dc678.chunk.js"
+    "revision": "f00ed0585114a84b9329",
+    "url": "/project-trybewallet-trybe/static/js/2.59a74a1c.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/project-trybewallet-trybe/static/js/2.6a1dc678.chunk.js.LICENSE.txt"
+    "url": "/project-trybewallet-trybe/static/js/2.59a74a1c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "341d878b6405180a4d4c",
-    "url": "/project-trybewallet-trybe/static/js/main.1a8ab7a0.chunk.js"
+    "revision": "dfd79d317ccb2a845311",
+    "url": "/project-trybewallet-trybe/static/js/main.7834eac4.chunk.js"
   },
   {
     "revision": "6503f4fa0966a5dcba5e",
